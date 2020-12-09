2d222c99076f31f1ed2f81955eb478ee2c0654c8
TemichT <temicht@gmail.com>
2020-12-09 09:55:45 +0300

    Home task №1

---
+++ Lesson 1 (08.12.20)/HomeTask_1/HomeTask_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"\nПривет, {name}, сегодня {DateTime.Now}");
+            Console.ReadLine();
+        }
+    }
+}
