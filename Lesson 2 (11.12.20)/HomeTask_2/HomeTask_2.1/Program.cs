5470634caf0d3ab87fb2c07dcb166dc6f55a605d
TemichT <temicht@gmail.com>
2020-12-12 16:50:06 +0300

    Home task №2.1

---
+++ Lesson 2 (11.12.20)/HomeTask_2/HomeTask_2.1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Домашнее задание 2.1
+/// </summary>
+namespace HomeTask_2._1
+{
+    class Program
+    {
+        #region Метод получения значения температуры из консоли
+        /// <summary>
+        /// Выводит значение в виде рационального числа с проверкой после ввода в консоли
+        /// </summary>
+        /// <param name="checkvalue">Максимальная величина модуля значения</param>
+        /// <returns></returns>
+        static double GetValue(double checkvalue)
+        {
+            double value;
+            string input = Console.ReadLine();
+            while (!double.TryParse(input.Replace('.', ','), out value) || (Math.Abs(value) > checkvalue))
+            {
+                if (Math.Abs(value) > checkvalue)
+                    Console.WriteLine("Вы ввели нереальное значение, пожалуйста введите снова:");
+                else
+                    Console.WriteLine("Вы ввели неправильное значение, пожалуйста введите снова:");
+                input = Console.ReadLine();
+            }
+            return value;
+        }
+        #endregion
+        /// <summary>
+        /// Вычисление средней температуры за сутки на основе мин. и макс. значений, введенных пользователем
+        /// </summary>
+        static void Main()
+        {
+            // Запрос минимальной температуры за сутки
+            Console.WriteLine("Пожалуйста введите минимальную температуру за сутки, градусы Цельсия:");
+            double minTemperature = GetValue(100.0);
+
+            // Запрос максимальной температуры за сутки
+            Console.WriteLine("Пожалуйста введите максимальную температуру за сутки, градусы Цельсия:");
+            double maxTemperature = GetValue(100.0);
+
+            //Расчет среднего значения температуры за сутки
+            double averageTemperature = (maxTemperature + minTemperature) / 2;
+
+            //Вывод результатов на экран
+            Console.Clear();
+            Console.WriteLine($"Минимальная температура за сутки: {minTemperature} град. Цельсия;");
+            Console.WriteLine($"Максимальная температура за сутки: {maxTemperature} град. Цельсия;");
+            Console.WriteLine($"\nСредняя температура за сутки: {averageTemperature} град. Цельсия.");
+
+            Console.ReadLine();
+        }
+    }
+}
