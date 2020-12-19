9ef80d99099408d238ce0d0b987028e8605ddbdd
TemichT <temicht@gmail.com>
2020-12-19 13:46:41 +0300

    Home task №4.1

---
+++ Lesson 4 (18.12.20)/HomeTask_4/HomeTask_4.1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Массив из кортежей, содержащих отдельные записи фамилии, имени и отчества
+            (string firstName, string lastName, string patronymic)[] names = 
+             new (string firstName, string lastName, string patronymic)[] 
+                {
+                    ("Иван", "Иванов", "Иванович"),
+                    ("Петр", "Петров", "Петрович"),
+                    ("Сидор", "Сидоров", "Сидорович"),
+                    ("Игорь", "Кузьмин", "Валентинович"),
+                    ("Полиграф", "Шариков", "Полиграфович"),
+                };
+
+            Console.WriteLine("Полное ФИО:");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(GetFullName(names[i].firstName, names[i].lastName, names[i].patronymic));
+            }
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Формирует полное ФИО из отдельных элементов
+        /// </summary>
+        /// <param name="firstName">Имя</param>
+        /// <param name="lastName">Фамилия</param>
+        /// <param name="patronymic">Отчество</param>
+        /// <returns>ФИО ввиде строкового значения</returns>
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return ($"{lastName} {firstName} {patronymic}");
+        }
+    }
+}
