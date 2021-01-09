af04b8afbfc7084620cf05afa42abb7ed9c216d7
Temich_Tirm <temich@gmail.com>
2021-01-09 16:39:10 +0300

    Home task №8

---
+++ Lesson 8 (30.12.20)/HomeTask_8/HomeTask_8.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings.Default.greeting);
+
+            if (string.IsNullOrEmpty(Properties.Settings.Default.userName))
+            {
+                Console.WriteLine("\nПожалуйста введите своё имя:");
+                Properties.Settings.Default.userName = Console.ReadLine();
+                Console.WriteLine("\nПожалуйста введите свой возраст:");
+                while (true)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int userAge))
+                    {
+                        if ((userAge > 0) && (userAge <= 120))
+                        {
+                            Properties.Settings.Default.userAge = userAge;
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Вы ввели неверные данные, пожалуйста попробуйте снова:");
+                }
+                Console.WriteLine("\nПожалуйста введите свой род деятельности:");
+                Properties.Settings.Default.userJob = Console.ReadLine();
+                Properties.Settings.Default.Save();
+                Console.WriteLine("Ваши данные успешно сохранены в настройках.");
+            }
+            else
+            {
+                string userName = Properties.Settings.Default.userName;
+                int userAge = Properties.Settings.Default.userAge;
+                string userJob = Properties.Settings.Default.userJob;
+                Console.WriteLine($"{Properties.Settings.Default.userGreeting} {userName}");
+                Console.WriteLine($"Ваш возраст: {userAge}");
+                Console.WriteLine($"Ваша род деятельности: {userJob}");
+            }
+            Console.ReadLine();
+            //Properties.Settings.Default.Reset();            
+        }
+    }
+}
---
+++ Lesson 8 (30.12.20)/HomeTask_8/HomeTask_8.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HomeTask№8")]
+[assembly: AssemblyDescription("Домашнее задание к уроку №8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Тирмяев А.А.")]
+[assembly: AssemblyProduct("HomeTask_8.1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1a71b840-2799-4c8a-962a-a1867d36aa74")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("1.2.3.4")]
