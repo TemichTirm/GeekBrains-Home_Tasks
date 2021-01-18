32f3525490b6af602677156e7430a209d8877fbc
Temich_Tirm <temich@gmail.com>
2021-01-19 00:48:01 +0300

    Home task №2.2

---
+++ Stage 2/Lesson 2 (15.01.21)/HomeTask_2/HomeTask_2.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 2, 5, 6, 9, 1, 7, 3, 8, 4, 10 };
+            int result = (BinarySearch(15, array));
+            Console.WriteLine((result == -1) ? "Такого элемента нет!" : $"{result}");
+            foreach (int a in array)
+                Console.Write($"{a}, ");
+            Console.ReadLine();
+        }
+        static int BinarySearch(int searchValue, int[] array)
+        {
+            if (array == null) return -1;
+            Array.Sort(array);
+            int minIndex = 0;
+            int maxIndex = array.Length - 1;
+            while(minIndex <= maxIndex)
+            {
+                int midIndex = (maxIndex + minIndex) / 2;
+                if (array[midIndex] == searchValue) return midIndex;
+                else
+                {
+                    if (array[midIndex] > searchValue) maxIndex = midIndex - 1; 
+                    else minIndex = midIndex + 1;
+                }
+            }
+            return -1;
+            // Асимптотическая сложность бинарного поиска О(log(N) по основанию 2)
+        }
+    }
+}
