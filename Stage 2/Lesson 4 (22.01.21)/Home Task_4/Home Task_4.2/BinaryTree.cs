d54493ca644ef61ade38125666a6b77d4242a0d2
Temich_Tirm <temich@gmail.com>
2021-02-01 01:31:46 +0300

    Home task №4 final

--- Stage 2/Lesson 4 (22.01.21)/Home Task_4/Home Task_4.2/BinaryTree.cs
+++ Stage 2/Lesson 4 (22.01.21)/Home Task_4/Home Task_4.2/BinaryTree.cs
@@ -9,105 +9,327 @@ namespace Home_Task_4._2
 {
     class BinaryTree
     {
-        private static int NodeCount { get; set; }
-        private static int Layers { get; set; }
-        //private static int currentLayerCount = 0;
-        private const int cellSize = 4;
+        private int NodeCount { get; set; }
+        private int TreeHeight { get; set; }
+        private const int cellSize = 4; // Размер строки для отображения узла дерева два символа на скобки идва на само число
         public Node RootNode { get; set; }
-        public void AddNode(int value, Node root = null, int currentLayerCount = 0)
+        /// <summary>
+        /// Добавление узла в дерево
+        /// </summary>
+        /// <param name="value">Значение нового узла</param>
+        public void Add(int value)
         {
             if (RootNode == null)
             {
-                Node newNode = new Node(value);
-                RootNode = newNode;
+                RootNode = new Node(value);
                 NodeCount++;
-                currentLayerCount++;
+                TreeHeight++;
             }
             else
             {
-                root = root ?? RootNode;
-                currentLayerCount++;
-                if (root.Value > value)
+                Add(value, RootNode);
+            }
+        }
+        /// <summary>
+        /// Добавление узла, если корневой узел уже существует
+        /// </summary>
+        /// <param name="value">Значениенового узла</param>
+        /// <param name="root">Корневой узел</param>
+        /// <param name="currentLayerCount"></param>
+        private void Add(int value, Node root, int currentLayerCount = 1)
+        {
+            if ( value < root.Value)
+            {
+                if (root.LeftNode == null)
                 {
-                    if (root.LeftNode == null)
-                    {
-                        Node newNode = new Node(value);
-                        root.LeftNode = newNode;
-                        newNode.ParentNode = root;
-                        NodeCount++;
-                        currentLayerCount++;
-                    }
-                    else
-                        AddNode(value, root.LeftNode, currentLayerCount);
+                    root.LeftNode = new Node(value) {ParentNode = root};
+                    NodeCount++;
+                    currentLayerCount++;
                 }
                 else
-                {
-                    if (root.RightNode == null)
-                    {
-                        Node newNode = new Node(value);
-                        root.RightNode = newNode;
-                        newNode.ParentNode = root;
-                        NodeCount++;
-                        currentLayerCount++;
-                    }
-                    else
-                        AddNode(value, root.RightNode, currentLayerCount);
-                }
+                    Add(value, root.LeftNode, currentLayerCount + 1);
             }
-            Layers = currentLayerCount > Layers ? currentLayerCount : Layers;
+            if (value > root.Value)
+                {
+                if (root.RightNode == null)
+                {
+                    root.RightNode = new Node(value) {ParentNode = root};
+                    NodeCount++;
+                    currentLayerCount++;
+                }
+                else
+                    Add(value, root.RightNode, currentLayerCount + 1);
+            }
+            TreeHeight = currentLayerCount > TreeHeight? currentLayerCount : TreeHeight;
         }
+        /// <summary>
+        /// Поиск значения в дереве
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <returns>True - значение найдено, False - значение не найдено</returns>
+        public bool Find(int value)
+        {
+            bool result = FindNode(value, RootNode) != null;
+            return result;
+        }
+        /// <summary>
+        /// Рекурсивный поиск узла по значению в дереве
+        /// </summary>
+        /// <param name="value">Искомое значение</param>
+        /// <param name="root">Стартовый узел</param>
+        /// <returns>Узел с искомым значением</returns>
+        private Node FindNode(int value, Node root)
+        {
+            if (value == root.Value)
+                return root;
+            else if (value < root.Value && root.LeftNode != null)
+                return FindNode(value, root.LeftNode);
+            else if (value > root.Value && root.RightNode != null)
+                return FindNode(value, root.RightNode);
+            else return null;
+        }
+        /// <summary>
+        /// Удаление узла с заданным значением
+        /// </summary>
+        /// <param name="value">Значение узла, который нужно удалить</param>
+        public void Delete(int value)
+        {
+            Node deleteNode = FindNode(value, RootNode);
+            // Если узел с искомым значением отсутствует
+            if (deleteNode == null)
+            {
+                Console.WriteLine("Нет узла с указанным значением");
+                Console.ReadLine();
+            }
+            else
+            {
+                #region Если у удаляемого узла нет потомка справа
+                // До
+                //       10(this)     
+                //      /     
+                //     8     
+                //    /         
+                //   5     
+                //     
+                // После     
+                //       8     
+                //      /     
+                //     5  
+                #endregion
+                if (deleteNode.RightNode == null)
+                {
+                    // Если удаляемый узел является корнем дерева
+                    if (deleteNode == RootNode)
+                    {
+                        if (deleteNode.LeftNode != null)
+                        {
+                            deleteNode.LeftNode.ParentNode = null;
+                            RootNode = deleteNode.LeftNode;
+                        }
+                        else RootNode = null;
+                    }
+                    // Если удаляемый узел не является корнем дерева
+                    else
+                    {
+                        if (deleteNode.ParentNode.LeftNode == deleteNode)
+                            deleteNode.ParentNode.LeftNode = deleteNode.LeftNode;
+                        else
+                            deleteNode.ParentNode.RightNode = deleteNode.LeftNode;
+                    }
+                    NodeCount--;
+                }
+                #region Если у удаляемого узла есть потомок справа, у которого нет потомков слева
+                // До
+                //       10(this)     
+                //      /  \  
+                //     8    15 
+                //    /         
+                //   5     
+                //     
+                // После  
+                //         15
+                //        /
+                //       8     
+                //      /     
+                //     5  
+                #endregion
+                else if (deleteNode.RightNode.LeftNode == null)
+                {
+                    // Если удаляемый узел является корнем дерева
+                    if (deleteNode == RootNode)
+                    {                        
+                        RootNode = deleteNode.RightNode;
+                        RootNode.LeftNode = deleteNode.LeftNode;
+                    }
+                    // Если удаляемый узел не является корнем дерева
+                    else
+                    {
+                        if (deleteNode.ParentNode.LeftNode == deleteNode)
+                            deleteNode.ParentNode.LeftNode = deleteNode.RightNode;
+                        else
+                            deleteNode.ParentNode.RightNode = deleteNode.RightNode;
+                    }
+                    deleteNode.RightNode.LeftNode = deleteNode.LeftNode;
+                    deleteNode.RightNode.ParentNode = deleteNode.ParentNode;
+                    if (deleteNode.LeftNode != null)
+                            deleteNode.LeftNode.ParentNode = deleteNode.RightNode;
+                    NodeCount--;
+                }
+                #region Если у удаляемого узла есть потомок справа, у которого есть потомки слева
+                // До
+                //       10(this)     
+                //      /  \  
+                //     8    15 
+                //    /    /    
+                //   5    13
+                //     
+                // После  
+                //         13
+                //        /  \
+                //       8    15  
+                //      /     
+                //     5  
+                #endregion
+                else
+                {
+                    Node insertNode = FindLeft(deleteNode.RightNode);
+                    if (insertNode.RightNode != null)
+                        insertNode.RightNode.ParentNode = insertNode.ParentNode;
+                    insertNode.ParentNode.LeftNode = insertNode.RightNode;
+                    deleteNode.Value = insertNode.Value;
+                    NodeCount--;
+                }
+                TreeHeight = 0;
+                CheckTreeHeight(RootNode);
+            }
+        }
+        /// <summary>
+        /// Поиск самого левого потомка длязаданного узла
+        /// </summary>
+        /// <param name="root">Заданный узел</param>
+        /// <returns>Самый левый потомок</returns>
+        private Node FindLeft(Node root)
+        {
+            if (root.LeftNode == null)
+                return root;
+            else return FindLeft(root.LeftNode);
+        }
+        /// <summary>
+        /// Вывод дерева в консоль
+        /// </summary>
         public void Print()
         {
+            if (RootNode == null)
+                return;
             Node root = RootNode;
+            // Создание двумерного массива строк размерность которого зависит от уровней дерева 
+            string[,] stringTree = new string[TreeHeight * 2 - 1, (int)Math.Pow(2, TreeHeight)];
+            // Текущая строка в которой должен находится значение узла
             int currentline = 0;
-            string[,] stringTree = new string[Layers * 2 - 1, (int)Math.Pow(2, Layers)];
-            int column = (int)Math.Pow(2, Layers - 1);
+            // Текущая колонка в которой должен находится значение узла
+            int column = (int)Math.Pow(2, TreeHeight - 1);
+            // Сдвиг по горизонтали от текущей колонки
             int offset = 0;
+            // Заполнение двумерного массива строк значениями элементов дерева и линиями
             AddValueInStringTree(root, currentline, stringTree, column, offset);
 
             Console.WriteLine($"Количество элементов: {NodeCount}");
-            Console.WriteLine($"Высота дерева: {Layers}\n");
+            Console.WriteLine($"Высота дерева: {TreeHeight}\n");
 
+            // Формирование строк из подстрок двумерного массива для вывода в консоль
             StringBuilder[] finalTree = new StringBuilder[stringTree.GetLength(0)];
             for (int i = 0; i < stringTree.GetLength(0); i++)
             {
                 finalTree[i] = new StringBuilder();
                 for (int j = 0; j < stringTree.GetLength(1); j++)
                 {
+                    // При отсутствии значения в ячейке двумерного массива, ячейка заполняется пустой строкой из пробелов
                     if (stringTree[i, j] == null)
+                    {
                         stringTree[i, j] = new string(' ', cellSize);
+                    }
                     finalTree[i].Append(stringTree[i, j]);
                 }
                 Console.WriteLine(finalTree[i]);
-                Thread.Sleep(300);
+                Thread.Sleep(100);
             }
         }
+        /// <summary>
+        /// Заполнение двумерного массива строк значениями элементов дерева и линиями
+        /// </summary>
+        /// <param name="root">Корневой узел</param>
+        /// <param name="currentLine">Текущая строка массива</param>
+        /// <param name="tree">Двумерный массив строк дерева</param>
+        /// <param name="column">Текущая колонка массива</param>
+        /// <param name="offset">Смещение по горизонтали</param>
         private void AddValueInStringTree(Node root, int currentLine, string[,] tree, int column, int offset)
         {
             column += offset;
-            tree[currentLine * 2, column] = string.Format("({0,2})", root.Value);
+            // Добавление значения узла дерева в массив
+            tree[currentLine * 2, column] = string.Format("[{0,2}]", root.Value);
             currentLine++;
-            offset = (int)Math.Pow(2, (Layers - 1) - currentLine);
-
+            // Расчет смещения для элементов следующего уровня
+            offset = (int)Math.Pow(2, (TreeHeight - 1) - currentLine);
+            // Проход по левому поддереву
             if (root.LeftNode != null)
             {
                 for (int i = column - 1; i > column - offset; i--)
                 {
+                    // Вывод горизонтальных линий
                     tree[(currentLine-1) * 2, i] = new string('_', cellSize);
                 }
+                // Вывод вертикальных линий 
                 tree[(currentLine) * 2 - 1, column - offset] = "   /";
+                // Рекурсивный переход на следующий уровень
                 AddValueInStringTree(root.LeftNode, currentLine, tree, column, -offset);
             }
+            // Проход по правому поддереву
             if (root.RightNode != null)
             {
                 for (int i = column + 1; i < column + offset; i++)
                 {
+                    // Вывод горизонтальных линий
                     tree[(currentLine - 1) * 2, i] = new string('_', cellSize);
                 }
+                // Вывод вертикальных линий 
                 tree[(currentLine) * 2 - 1, column + offset] = "\\   ";
+                // Рекурсивный переход на следующий уровень
                 AddValueInStringTree(root.RightNode, currentLine, tree, column, offset);
             }
             else return;
+        }
+        /// <summary>
+        /// Рекурсивная проверка высоты дерева. Необходима после операции удаления для корректного вывода в консоль
+        /// </summary>
+        /// <param name="root">Корневой узел</param>
+        /// <param name="currentLayerCount">Номер текущего уровня</param>
+        private void CheckTreeHeight(Node root, int currentLayerCount = 0)
+        {
+            if (RootNode != null)
+            {
+                currentLayerCount++;
+                if (root.LeftNode != null)
+                {
+                    CheckTreeHeight(root.LeftNode, currentLayerCount);
+                }
+                if (root.RightNode != null)
+                {
+                    CheckTreeHeight(root.RightNode, currentLayerCount);
+                }
+                TreeHeight = currentLayerCount > TreeHeight ? currentLayerCount : TreeHeight;
+            }
+            else TreeHeight = 0;
+        }
+        /// <summary>
+        /// Вывод меню в консоль
+        /// </summary>
+        public static void PrintMenu()
+        {
+            Console.WriteLine("\nДля изменения дерева выберите операцию" +
+                              "\n1 - Добавить элемент" +
+                              "\n2 - Удалить элемент\n" +
+                              "\n0 - Выход");
+            
         }
     }
 }
--- Stage 2/Lesson 4 (22.01.21)/Home Task_4/Home Task_4.2/Program.cs
+++ Stage 2/Lesson 4 (22.01.21)/Home Task_4/Home Task_4.2/Program.cs
@@ -11,19 +11,63 @@ namespace Home_Task_4._2
         static void Main(string[] args)
         {
             BinaryTree binaryTree = new BinaryTree();
-            binaryTree.AddNode(10);
-            binaryTree.AddNode(8);
-            binaryTree.AddNode(15);
-            binaryTree.AddNode(6);
-            binaryTree.AddNode(7);
-            binaryTree.AddNode(9);
-            binaryTree.AddNode(5);
-            binaryTree.AddNode(13);
-            binaryTree.AddNode(20);
-            binaryTree.AddNode(30);
-            Console.Read();
-            binaryTree.Print();
-            Console.ReadLine();
+            #region Предварительное наполнение дерева элементами
+            //binaryTree.Add(11);
+            //binaryTree.Add(15);
+            //binaryTree.Add(7);
+            //binaryTree.Add(6);
+            //binaryTree.Add(9);
+            //binaryTree.Add(7);
+            //binaryTree.Add(8);
+            //binaryTree.Add(5);
+            //binaryTree.Add(12);
+            //binaryTree.Add(25);
+            //binaryTree.Add(30);
+            //binaryTree.Add(14);
+            //binaryTree.Add(19);
+            //binaryTree.Add(10);
+            #endregion
+            int choice = 1;
+            int minValue = 0;
+            int maxValue = 99;
+            while (choice != 0)
+            {
+                binaryTree.Print();
+                BinaryTree.PrintMenu();
+                choice = GetValue(0, 2);
+                switch (choice)
+                {
+                    case 0:
+                        break;
+                    case 1:         // Добавление элемента
+                        Console.WriteLine($"Введите значение, которое хотите добавить в диапазоне от {minValue} до {maxValue}");
+                        binaryTree.Add(GetValue(minValue, maxValue));
+                        break;
+                    case 2:         // Удаление элемента
+                        Console.WriteLine($"Введите значение, которое хотите удалить в диапазоне от {minValue} до {maxValue}");
+                        binaryTree.Delete(GetValue(minValue, maxValue));
+                        break;
+                }
+                Console.Clear();       
+            }
+        }
+        /// <summary>
+        /// Выводит значение в виде числа типа Int от минимального до максимального значения включительно с проверкой после ввода в консоли 
+        /// </summary>
+        /// <param name="minValue">Минимальная величина значения</param>
+        /// <param name="maxValue">Максимальная величина значения</param>
+        /// <returns></returns>
+        private static int GetValue(int minValue, int maxValue)
+        {
+            int value;
+            string input = Console.ReadLine();
+            int line = Console.CursorTop;
+            while (!int.TryParse(input, out value) || (value > maxValue) || (value < minValue))
+            {
+                Console.Write($"Ошибка! Вы ввели неверное значение, пожалуйста введите число от {minValue} до {maxValue}:");
+                input = Console.ReadLine();
+            }
+            return value;
         }
     }
 }
