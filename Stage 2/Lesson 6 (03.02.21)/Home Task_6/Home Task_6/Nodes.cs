ccb01d86fd113f2c9305db81cdbff1decefb311b
Temich <temicht@gmail.com>
2021-02-14 15:21:26 +0300

    Home task№6 (final)

--- Stage 2/Lesson 6 (03.02.21)/Home Task_6/Home Task_6/Nodes.cs
+++ Stage 2/Lesson 6 (03.02.21)/Home Task_6/Home Task_6/Nodes.cs
@@ -9,6 +9,16 @@ namespace Home_Task_6
     class Nodes
     {
         public int Num { get; set; }
-        public List<Edges> Links { get; set; }
+        public List<Edges> Edges { get; set; }
+        public int Value { get; set; }
+        public Nodes()
+        {
+            Edges = new List<Edges>();
+        }
+    }
+    class Edges
+    {
+        public Nodes Node { get; set; }
+        public int Weight { get; set; }
     }
 }
